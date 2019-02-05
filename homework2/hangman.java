ffd0a65f189ef73cf21de1202a463d02be2fba6b
nathan416 <30889482+nathan416@users.noreply.github.com>
2019-02-05 10:31:29 -0800

    added displayHangman()

--- homework2/hangman.java
+++ homework2/hangman.java
@@ -4,11 +4,53 @@ Due Date: 2/11/2019
 Names:Nathan Flack
 ********************/
 import java.util.*;
+import javax.swing.JOptionPane;
 
 public class hangman
 {
 	public static void main(String [] args)
 	{
+		displayHangman(6);
+	}
 
+	public static void menu()
+	{
+
+	}
+
+	public static void displayHangman(int strikes)
+	{
+		if (strikes == 0)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |\n       |\n       |\n___|___\n");
+		}
+		else if (strikes == 1)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |\n       |\n___|___\n");
+		}
+		else if (strikes == 2)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |                |\n       |\n___|___\n");
+		}
+		else if (strikes == 3)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |               /|\n       |\n___|___\n");
+		}
+		else if (strikes == 4)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |               /|\n       |                 \\\n___|___\n");
+		}
+		else if (strikes == 5)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |               /|\\\n       |                 \\\n___|___\n");
+		}
+		else if (strikes == 6)
+		{
+			JOptionPane.showMessageDialog(null,"       ________\n       |                 |\n       |                0\n       |               /|\\\n       |               /\\\n___|___\n");
+		}
+		else
+		{
+
+		}
 	}
 }
