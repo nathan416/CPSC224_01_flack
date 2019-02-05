821b3e72b4660aed09d82b5eabe004f5a0e5d7ae
nathan416 <30889482+nathan416@users.noreply.github.com>
2019-02-05 09:33:53 -0800

    started file

---
+++ homework2/hangman.java
@@ -0,0 +1,14 @@
+/***************
+Homework #2
+Due Date: 2/11/2019
+Names:Nathan Flack
+********************/
+import java.util.*;
+
+public class hangman
+{
+	public static void main(String [] args)
+	{
+
+	}
+}
