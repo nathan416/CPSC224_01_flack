1551a414d301e36610ae3d487c7f00347125bc29
nathan416 <30889482+nathan416@users.noreply.github.com>
2019-02-22 22:05:24 -0800

    finished action listener

--- ICA4/DieRoll.java
+++ ICA4/DieRoll.java
@@ -3,11 +3,10 @@ ICA #4
 Due Date: 2/24/2019
 Names:Nathan Flack
 ********************/
+import java.util.*;
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-
-
 
 public class DieRoll extends JFrame
 {
@@ -16,8 +15,7 @@ public class DieRoll extends JFrame
 	private final int WINDOW_WIDTH = 310;  // Window width
 	private final int WINDOW_HEIGHT = 310;
 
-	private JButton rightDie;
-	private JButton leftDie;
+	private JButton rollButton;
 
 	ImageIcon Die1 = new ImageIcon("Die1.png");
 	ImageIcon Die2 = new ImageIcon("Die2.png");
@@ -25,6 +23,8 @@ public class DieRoll extends JFrame
 	ImageIcon Die4 = new ImageIcon("Die4.png");
 	ImageIcon Die5 = new ImageIcon("Die5.png");
 	ImageIcon Die6 = new ImageIcon("Die6.png");
+
+	Random rand;
 
 	public DieRoll()
 	{
@@ -49,25 +49,72 @@ public class DieRoll extends JFrame
 
 	private void buildPanel()
 	{
-		leftDie = new JButton("");
- 	   	rightDie = new JButton("");
+		rollButton = new JButton("Roll the Dice");
 
-		leftDie.addActionListener(new TicTacToeListener());
- 	   	rightDie.addActionListener(new TicTacToeListener());
+		rollButton.addActionListener(new DieRollListener());
 
 		panel = new JPanel();
 
 		panel.setLayout(new GridLayout(3,3));
 
-		panel.add(leftDie);
-		panel.add(rightDie);
+		panel.add(rollButton);
 	}
 
-	private class TicTacToeListener implements ActionListener
+	private class DieRollListener implements ActionListener
     {
        public void actionPerformed(ActionEvent e)
        {
-
+		   	rand = new Random();
+		   	int n = rand.nextInt(5);
+		   	int m = rand.nextInt(5);
+			if (n == 0)
+			{
+				rollButton.setIcon(Die1);
+			}
+			else if (n == 1)
+			{
+				rollButton.setIcon(Die2);
+			}
+			else if (n == 2)
+			{
+				rollButton.setIcon(Die3);
+			}
+			else if (n == 3)
+			{
+				rollButton.setIcon(Die4);
+			}
+			else if (n == 4)
+			{
+				rollButton.setIcon(Die5);
+			}
+			else if (n == 5)
+			{
+				rollButton.setIcon(Die6);
+			}
+			if (m == 0)
+			{
+				rollButton.setIcon(Die1);
+			}
+			else if (m == 1)
+			{
+				rollButton.setIcon(Die2);
+			}
+			else if (m == 2)
+			{
+				rollButton.setIcon(Die3);
+			}
+			else if (m == 3)
+			{
+				rollButton.setIcon(Die4);
+			}
+			else if (m == 4)
+			{
+				rollButton.setIcon(Die5);
+			}
+			else if (m == 5)
+			{
+				rollButton.setIcon(Die6);
+			}
        }
     }
 
