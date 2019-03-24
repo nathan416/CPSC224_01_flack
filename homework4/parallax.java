a03b804e5232d301043ad6f1a6450463831f8af0
Evan-Swanson <44790747+Evan-Swanson@users.noreply.github.com>
2019-03-24 15:33:17 -0700

    added parallax effect

--- homework4/parallax.java
+++ homework4/parallax.java
@@ -10,13 +10,13 @@ import java.io.*;
 import javax.imageio.*;
 
 /**
-   This applet shows the mouse events as they occur.
+   This applet simulation motion parallax
 */
 
 public class parallax extends JApplet
 {
-   private int currentX = 0; // Mouse cursor's X position
-   private int currentY = 0; // Mouse cursor's Y position
+   private int currentX = 300; // Mouse cursor's X position (initialied to middle)
+   private int currentY = 200; // Mouse cursor's Y position
    private Image image1;
    private Image image2;
    private Image image3;
@@ -58,41 +58,32 @@ public class parallax extends JApplet
    {
       // Call the superclass's paint method.
       super.paint(g);
-      g.drawImage(image1, -40, -40, null);
-      g.drawImage(image2, -40, -40, null);
-      g.drawImage(image3, -40, -40, null);
-      g.drawImage(image4, -40, -40, null);
-      
-      //
+      //Paint each layer of the image with offset depending on mouse position
+      g.drawImage(image1, -100, -100, null);
+      g.drawImage(image2, (currentX / 150) - 80, (currentY / 150) - 100, null);
+      g.drawImage(image3, (currentX / 30) - 100, (currentY / 30) - 100, null);
+      g.drawImage(image4, (currentX / 5) - 100, (currentY / 5) - 100, null);
 
-      // Draw
-      //boolean Graphics.drawImage(Image img, int x, int y, ImageObserver observer);
    }
-   
    
    
    /**
       Private inner class that handles mouse events.
-      When an event occurs, the text field for that
-      event is given a yellow background.
    */
    
-   private class MyMouseListener
-                         implements MouseListener
+   private class MyMouseListener implements MouseListener
    {
-      public void mousePressed(MouseEvent e)
-      {
-
-      }
-
       public void mouseClicked(MouseEvent e)
       {
    
       }
-
+      public void mousePressed(MouseEvent e)
+      {
+   
+      }
       public void mouseReleased(MouseEvent e)
       {
-
+   
       }
 
       public void mouseEntered(MouseEvent e)
@@ -119,7 +110,10 @@ public class parallax extends JApplet
 
       public void mouseMoved(MouseEvent e)
       {
-
+         //Update draw position and redraw to screen
+         currentX = e.getX();
+         currentY = e.getY();
+         repaint();
       }
    }
 }
