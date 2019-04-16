3483243d0e5909d230015a296a40a37efe55d1c9
nathan416 <30889482+nathan416@users.noreply.github.com>
2019-04-16 09:55:59 -0700

    constructor and buildBejeweledPanel and added project .docx

--- FinalProject/bejeweled.java
+++ FinalProject/bejeweled.java
@@ -1,6 +1,7 @@
 /***************
 Final Project
-Due Date: 4//2019
+Demo Due Date: 4/19/2019
+Due Date: 5/7/2019
 Names:Nathan Flack, Evan Swanson
 ********************/
 import javax.swing.*;
@@ -8,3 +9,38 @@ import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
 import javax.imageio.*;
+
+public class bejeweled extends JFrame
+{
+
+	private JPanel bejeweledPanel;
+	private JPanel HighscorePanel;
+	private JPanel PointsPanel;
+
+	private final int WINDOW_WIDTH = 500;  // Window width
+	private final int WINDOW_HEIGHT = 500;
+
+	public bejeweled()
+	{
+		// Set the title.
+	 setTitle("Bejeweled");
+
+	 setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	 setLayout(new BorderLayout());
+
+	 buildBejeweledPanel();
+	 buildHighscorePanel();
+	 buildPointsPanel();
+	}
+
+	private void buildBejeweledPanel()
+	{
+		bejeweledPanel = new JPanel();
+		
+	}
+
+
+}
