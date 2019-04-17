e72f77e2e6d203b797d165c40e4bbdd44d50e9d5
Evan-Swanson <44790747+Evan-Swanson@users.noreply.github.com>
2019-04-17 15:18:45 -0700

    implemented paint function

--- FinalProject/bejeweled.java
+++ FinalProject/bejeweled.java
@@ -9,9 +9,10 @@ import java.awt.event.*;
 import java.awt.*;
 import java.io.*;
 import javax.imageio.*;
+import java.awt.image.*;
 import java.util.Random;
 
-public class bejeweled extends JFrame
+public class bejeweled extends JApplet
 {
 
 	private JPanel bejeweledPanel;
@@ -21,31 +22,71 @@ public class bejeweled extends JFrame
 	private int currentCol;
 	private int currentRow;
 	private char[][] boardInfo = new char[5][6];
+	private int x;
+	private int y;
 
 	private final int WINDOW_WIDTH = 720;  // Window width
 	private final int WINDOW_HEIGHT = 600;
 	private final int squareSize = 120;
+	private JFrame theFrame = new JFrame();
 
-	private Image background = new ImageIcon("background.jpg");
+	private Image background;
+	private Image redOrb;
+	private Image blueOrb;
+	private Image greenOrb;
+	private Image darkOrb;
+	private Image lightOrb;
+	private Image pinkOrb;
+
+	private BufferedImage bf;
+
 
 	public bejeweled()
 	{
 		// Set the title.
-	 setTitle("Bejeweled");
+	 theFrame.setTitle("Bejeweled");
 
-	 setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	 theFrame.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
 
-	 setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	 //theFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-	 setLayout(new BorderLayout());
+	 theFrame.setLayout(new BorderLayout());
+	 readImages();
 
 	 buildBejeweledPanel();
+
 	 //buildHighscorePanel();
 	 //buildPointsPanel();
 	 initializeBoard();
 
-	 add(bejeweledPanel);
+	 theFrame.add(bejeweledPanel);
+	  // Add a mouse listener to this applet.
+   addMouseListener(new boardListener());
+
+   // Add a mouse motion listener to this applet.
+   addMouseMotionListener(new boardMovementListener());
 	}
+
+
+
+  public void readImages () 
+  { 
+    try 
+    {                
+      background = ImageIO.read(new File("board.png"));
+      redOrb = ImageIO.read(new File("red.png"));
+      greenOrb = ImageIO.read(new File("green.png"));
+			blueOrb = ImageIO.read(new File("blue.png"));
+			darkOrb = ImageIO.read(new File("dark.png"));
+			lightOrb = ImageIO.read(new File("light.png"));
+			pinkOrb = ImageIO.read(new File("pink.png"));
+    } 
+    catch (IOException e) 
+    { 
+      //Not handled. 
+    } 
+  } 
+
 
 	private void buildBejeweledPanel()
 	{
@@ -53,14 +94,56 @@ public class bejeweled extends JFrame
 
 	}
 
-	public void paint(Graphics g)
-    {
+  public void update(Graphics g)
+	{
+		paint(g);
+	}
+
+	//build the buffered image
+	public void animation(Graphics g)
+	{
 		super.paint(g);
+		g.drawImage(background, 0, 0, null);
+
+		//draw each orb
+		for(int row = 0; row < 5; row++)
+		{
+			for(int col = 0; col < 6; col++)
+			{
+				if (boardInfo[row][col] == 'R')
+					g.drawImage(redOrb, col*squareSize, row*squareSize, null);
+				else if (boardInfo[row][col] == 'B')
+					g.drawImage(blueOrb, col*squareSize, row*squareSize, null);
+				else if (boardInfo[row][col] == 'G')
+					g.drawImage(greenOrb, col*squareSize, row*squareSize, null);
+				else if (boardInfo[row][col] == 'D')
+					g.drawImage(darkOrb, col*squareSize, row*squareSize, null);
+				else if (boardInfo[row][col] == 'L')
+					g.drawImage(lightOrb, col*squareSize, row*squareSize, null);
+				else if (boardInfo[row][col] == 'P')
+					g.drawImage(pinkOrb, col*squareSize, row*squareSize, null);
+			}
+		}
+	}
+
+	@Override
+	public void paint(Graphics g)
+  {
+		//use a buffered image to eliminate flickering
+		bf = new BufferedImage(720,600, BufferedImage.TYPE_INT_RGB);
+		try
+		{
+			animation(bf.getGraphics());
+			g.drawImage(bf,0,0,null);
+		}
+		catch(Exception ex)
+		{
+		}
 	}
 
 	private void initializeBoard()
 	{
-		char[] orbs = {'G','R','B','D','L','H'};
+		char[] orbs = {'G','R','B','D','L','P'};
 		Random rand = new Random();
 		int n = 0;
 
@@ -114,8 +197,12 @@ public class bejeweled extends JFrame
 	{
 		public void mouseMoved(MouseEvent e)
 		{
-			int x = e.getX();
-			int y = e.getY();
+		}
+
+		public void mouseDragged(MouseEvent e)
+		{
+			x = e.getX();
+			y = e.getY();
 
 			if(turnActive)
 			{
@@ -123,18 +210,17 @@ public class bejeweled extends JFrame
 				{
 					swap(currentRow, currentCol, currentRow, (x / squareSize));
 					currentCol = x / squareSize;
+					repaint();
 				}
 
 				if(currentRow != (y / squareSize))
 				{
-					swap(currentRow, currentCol, (y / squareSize), currentRow);
+					swap(currentRow, currentCol, (y / squareSize), currentCol);
 					currentRow = y / squareSize;
+					repaint();
 				}
 			}
-		}
-
-		public void mouseDragged(MouseEvent e)
-		{
+			
 		}
 
 	}
